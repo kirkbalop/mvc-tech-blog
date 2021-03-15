1188e9c8ea0cbd9998bbfa61c693991f30feff85
Kirk <kirkbalop@gmail.com>
2021-03-15 16:54:50 -0700

    updated routes/views

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 router.use('/api', apiRoutes);
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 // router.use('/dashboard', dashboardRoutes);
 router.use((req, res) => {
     res.status(404).end();
