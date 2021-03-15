b144d3f4646f6dc1d9af975aa7257ef1b9da8cef
Kirk <kirkbalop@gmail.com>
2021-03-14 18:11:59 -0700

    completed routers and indexes

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const routes = require('./controllers')
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({helpers});
 const session = require('express-session');
-const app = express();
-const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
@@ -17,6 +17,12 @@ const sess = {
         db: sequelize
     })
 };
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
