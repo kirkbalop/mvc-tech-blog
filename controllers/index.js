1db4a36fe9ae51703d77ea95bc0a0c781acb66e2
Kirk <kirkbalop@gmail.com>
2021-03-16 10:55:06 -0700

    login.js and completed connections

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-// router.use('/dashboard', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use((req, res) => {
     res.status(404).end();
 });
